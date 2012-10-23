0a8c79218c2eb73584786e6d8eb6f4aa4877da35
0x49D1 <0x49D1@localhost>
2012-10-23 14:48:48 +0400

    Renewed solution

---
+++ Smartfiction/DetailsView.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace Smartfiction
+{
+    public partial class DetailsView : PhoneApplicationPage
+    {
+        public DetailsView()
+        {
+            InitializeComponent();
+            this.Loaded += new RoutedEventHandler(DetailsView_Loaded);
+        }
+
+        void DetailsView_Loaded(object sender, RoutedEventArgs e)
+        {
+            string index = "";
+            if (NavigationContext.QueryString.TryGetValue("item", out index))
+            {
+                int _index = int.Parse(index);
+
+                webBrowser1.Navigate(new Uri(App.Model.FeedItems[_index].ItemLink));
+            }
+        }
+
+        private void webBrowser1_Navigating(object sender, NavigatingEventArgs e)
+        {
+            ProgBar.Visibility = Visibility.Visible;
+        }
+
+        private void webBrowser1_Navigated(object sender, System.Windows.Navigation.NavigationEventArgs e)
+        {
+            ProgBar.Visibility = Visibility.Collapsed;
+        }
+
+    }
+}
