ceacc49050573b2005e79dddcc986756314ee197
Dmitry Pursanov <dpursanov@live.com>
2013-01-03 14:54:13 +0400

    Added progress indicatorc

--- Smartfiction8/Smartfiction/App.xaml.cs
+++ Smartfiction8/Smartfiction/App.xaml.cs
@@ -1,15 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Animation;
+﻿using System.Windows;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
 using BugSense;
 using Microsoft.Phone.Controls;
 using Microsoft.Phone.Shell;
@@ -33,8 +23,6 @@ namespace Smartfiction
             BugSenseHandler.Instance.Init(this, "a9b557af");
             Data = new FeedHelper.FeedData();
             Data.FeedList = new ObservableCollection<string>();
-
-
 
             if (System.IO.IsolatedStorage.IsolatedStorageSettings.ApplicationSettings.Count != 0)
             {
